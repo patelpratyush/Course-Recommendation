3a04b6c4f8f0c7056f439df365d3cfb190077ada
Justin Tang Ly <92610733+Remonyz@users.noreply.github.com>
2024-06-21 15:51:26 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 import os
 import database
-import appy
 
 app = Flask(__name__)
 
@@ -22,28 +21,26 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        # Check if the post request has the file part
         if 'file' not in request.files:
             return redirect(request.url)
         file = request.files['file']
-        # If user does not select file, browser also
-        # submit an empty part without filename
         if file.filename == '':
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
-            # Process the transcript
             recommendations = process_transcript(file_path)
             return render_template('recommendations.html', recommendations=recommendations)
 
 def process_transcript(file_path):
     username = appy.extract_text_first_line(file_path)
     extracted_rows = appy.extract_rows_below_keyword(file_path, 'Subject')
+    
     if extracted_rows:
         subjects = []
         course_numbers = []
+        
         for item in extracted_rows:
             if item.isalpha():
                 subject = item
@@ -51,6 +48,7 @@ def process_transcript(file_path):
             else:
                 course_number = item
                 course_numbers.append(course_number)
+        
         combined_subject_course = [f"{subject} {course_number}" for subject, course_number in zip(subjects, course_numbers)]
         recommendations = database.recommendation(username)
         
@@ -63,6 +61,3 @@ def process_transcript(file_path):
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
-
-#delete transcripts after running
-#look into deployment 
